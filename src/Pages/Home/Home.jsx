fdad33184237d9deed97ce7259df22140f31bfc4
Sai Nikhil Naidu <91335522+Sainikhilnaidu9@users.noreply.github.com>
2024-01-07 09:24:54 +0530

    Product cards added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,33 +2,46 @@ import React from "react";
 import './Home.css';
 import Navbar from "../../components/Navbar";
 import ProductCard from "../../components/cards/ProductCard";
+import ProductImage from "../../components/Files/Channa-Masala-v1.png";
 
 function Home(){
     const products = [
         {
           id: 1,
           name: 'Product',
-          imageUrl: 'https://example.com/product1.jpg',
+          imageUrl: ProductImage,
           price: 13.99,
         },
         {
             id: 2,
           name: 'Product',
-          imageUrl: 'https://example.com/product1.jpg',
+          imageUrl: ProductImage,
           price: 13.95,
         },
         {
             id: 3,
           name: 'product',
-          imageUrl: 'https://example.com/product1.jpg',
+          imageUrl: ProductImage,
           price: 13.95,
         },
         {
             id: 4,
           name: 'Product',
-          imageUrl: 'https://example.com/product1.jpg',
+          imageUrl: ProductImage,
           price: 13.95,
-        }
+        },
+        {
+          id: 5,
+        name: 'Product',
+        imageUrl: ProductImage,
+        price: 13.95,
+      },
+      {
+        id: 6,
+      name: 'Product',
+      imageUrl: ProductImage,
+      price: 13.95,
+    }
       ];
     return (
         <div>
--- src/components/cards/ProductCard/ProductCard.jsx
+++ src/components/cards/ProductCard/ProductCard.jsx
@@ -1,12 +1,10 @@
-// ProductCard.js
 import React, { useState } from 'react';
 import {
   MDBCard,
   MDBCardBody,
   MDBCardImage,
-  MDBRipple,
 } from 'mdb-react-ui-kit';
-import './ProductCard.css'; // Import the CSS file
+import './ProductCard.css'; 
 
 const ProductCard = ({ product }) => {
     const [quantity, setQuantity] = useState(0);
@@ -36,13 +34,10 @@ const ProductCard = ({ product }) => {
   return (
     <MDBCard className="product-card">
       <MDBCardBody>
-        <MDBRipple
-          rippleColor="light"
-          rippleTag="div"
-          className="bg-image rounded hover-zoom hover-overlay"
-        >
-          <MDBCardImage src={product.imageUrl} fluid className="product-image" />
-        </MDBRipple>
+        <div className="cardbody">
+      <div className="bg-image rounded hover-zoom hover-overlay">
+      <MDBCardImage src={product.imageUrl} fluid className="product-image" />
+    </div>
 
         <div className="product-details">
           <h5 className="product-name">{product.name}</h5>
@@ -53,7 +48,7 @@ const ProductCard = ({ product }) => {
               <button className="btn btn-outline-primary btn-sm me-2" onClick={handleDecrement}>
                 -
               </button>
-              <span>{quantity}</span>
+              <span className='quantity'>{quantity}</span>
               <button className="btn btn-outline-primary btn-sm ms-2" onClick={handleIncrement}>
                 +
               </button>
@@ -61,10 +56,12 @@ const ProductCard = ({ product }) => {
           )}
         </div>
 
+        {quantity ===0 && (
         <div className="add-to-cart">
-          <button className='addToCart' color="primary" size="sm" onClick={handleAddToCart} disabled={isButtonDisabled}>
+          <button className='button-37' onClick={handleAddToCart} disabled={isButtonDisabled}>
             Add to Cart
           </button>
+        </div>)}
         </div>
       </MDBCardBody>
     </MDBCard>

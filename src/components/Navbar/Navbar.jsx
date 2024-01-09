d9ca71785ce1243265310738dd1f758ab600258a
Sai Nikhil Naidu <91335522+Sainikhilnaidu9@users.noreply.github.com>
2024-01-09 22:37:14 +0530

    Resolved merge conflicts

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-// Navbar.js
-import React from 'react';
-import Logo from '../Requires/Logo';
-import './Navbar.css';
-=======
 import React, { useState, useEffect } from 'react';
 import Logo from '../Requires/Logo';
 import UserDetailsTable from '../GodownDetails/UserDetailsTable';
 import './Navbar.css';
+  
 
 function Navbar() {
   const [users, setUsers] = useState([]);
@@ -26,40 +21,7 @@ function Navbar() {
     const selectedUser = users.find(user => user.id === parseInt(selectedUserId, 10));
     setSelectedUser(selectedUser);
   };
->>>>>>> 2c1a8c26560297abec6ba705a6b39d0f139da32f
-
-function Navbar() {
   return (
-<<<<<<< HEAD
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Logo />
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/Products">Products</a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" href="/cart" tabIndex="-1">Cart</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/management" tabIndex="-1">Management</a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-=======
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Logo />
@@ -135,7 +97,6 @@ function Navbar() {
       {/* Render User Details Table below the Navbar */}
       <UserDetailsTable selectedUser={selectedUser} />
     </>
->>>>>>> 2c1a8c26560297abec6ba705a6b39d0f139da32f
   );
 }
 

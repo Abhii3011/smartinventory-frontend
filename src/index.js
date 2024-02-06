5279f33dd4ab190aeb7580e0335624facee2bebd
Abhii3011 <sahaiabhinav7@gmail.com>
2024-02-06 15:51:25 +0530

    commits

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  // <React.StrictMode>
+  //   <App />
 
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 
